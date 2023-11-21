0844dae3f0f37c1a681076a0958368293584851a
MostafaHussinZaki <mostafahussinzaki603@gmail.com>
2023-11-21 21:03:04 +0200

    html scss without loading and error messages

---
+++ dist/index.c5872702.js
@@ -0,0 +1,11 @@
+// making the search Bar
+const searchbar = document.querySelector(".header__option__search-bar__input");
+const img = document.querySelector(".header__option__search-bar__img");
+searchbar.addEventListener("focus", function() {
+    img.style.left = "-40px";
+});
+searchbar.addEventListener("focusout", function() {
+    img.style.left = "7px";
+});
+
+//# sourceMappingURL=index.c5872702.js.map
